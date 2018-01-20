d8660f712079e5f963783364782a30b5128aabd5
Anatoli Kuzmiankou <Anatoli_Kuzmiankou@epam.com>
2018-01-20 17:06:09 +0300

    Fix base skeleton

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>header</header>
+        <nav>nav</nav>
+        <main>main</main>
+        <footer>footer</footer>
+      </div>
+    );
+  }
+}
+
+export default App;
