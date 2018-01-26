79b128a9812103c94fd83e257c4cfe27a8cf1950
Anatoli Kuzmiankou <Anatoli_Kuzmiankou@epam.com>
2018-01-26 16:42:24 +0300

    Fix user navigation

--- src/components/GenreNavigator.js
+++ src/components/GenreNavigator.js
@@ -1,20 +1,45 @@
 import React, { Component } from 'react';
 import { getGenreList } from '../lib/manga-scraper-request';
+import Loading from './Loading';
+import Item from './MenuItem';
 
 class GenreNavigator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true
+    }
+  }
+  
   componentDidMount() {
     getGenreList()
-      .then(json => console.log(json));
+      .then(json => {
+        this.genreList = json.map( item => item.genreId );
+        this.setState({ loading: false });
+      });
+  }
+
+  createLoadinContainer() {
+    return (
+      <Loading className='loading genre-navigator-loading' />
+    );
+  }
+
+  createItems(list) {
+    return list.map(item => <Item key={ item } href={ `/manga-list/${ item }` } className={ `genre genre-${ item }` }>{ item }</Item>)
   }
   
   render() {
     return (
       <div>
-        <ul>
-          <li><a href="#">Item 1</a></li>
-          <li><a href="#">Item 2</a></li>
-          <li><a href="#">Item 3</a></li>
-        </ul>
+        { this.state.loading
+            ? <Loading />
+            : (
+              <ul>
+                <Item href="/manga-list" className='genre genre-all'> all </ Item>
+                { this.createItems(this.genreList) }
+              </ul>
+            )}
       </div>
     );
   }
