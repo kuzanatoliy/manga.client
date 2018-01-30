cec2d2ee9b1cd42c9352562f1db11773b88ab44c
Anatoli Kuzmiankou <Anatoli_Kuzmiankou@epam.com>
2018-01-30 13:28:29 +0300

    Fix link

--- src/components/Navigator.js
+++ src/components/Navigator.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { HOME_URL, SEARCH_URL } from '../constants/urls';
+import { CATALOG_URL, SEARCH_URL } from '../constants/urls';
 import Item from './MenuItem';
 
 function Navigator() {
   return (
     <nav className='navigator'>
       <ul className='menu'>
-        <Item href={ HOME_URL }>Home</Item>
+        <Item href={ CATALOG_URL }>Catalog</Item>
         <Item href={ SEARCH_URL }>Search</Item>
       </ul>
     </nav>
