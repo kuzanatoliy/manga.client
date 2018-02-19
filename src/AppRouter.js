51d3fb164ac906a9b992be7de60f94904d904444
Anatoli Kuzmiankou <Anatoli_Kuzmiankou@epam.com>
2018-02-19 12:50:40 +0300

    Fix styles

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import { HOME_URL, CATALOG_URL, SEARCH_URL, MANGA_URL } from './constants/urls';
-import { CatalogPage } from './pages';
+import CatalogPage from './pages/CatalogPage';
 import Main from './components/Main';
 
 function AppRouter() {
@@ -12,8 +12,6 @@ function AppRouter() {
           <Redirect exact from={ HOME_URL } to={ CATALOG_URL } />
           <Route exact path={ `${ CATALOG_URL }/:genreId` } component={ CatalogPage } />
           <Route exact path={ CATALOG_URL } component={ CatalogPage }/>
-          <Route exact path={ SEARCH_URL } render={ () => <div>SEARCH</div> } />
-          <Route exact path={ MANGA_URL } render={ () => <div>MANGA</div> } />
           <Route render={ () => <div>ERROR</div> } />
         </Switch>
       </Main>
