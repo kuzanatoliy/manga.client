50550858ef8ecaa3cf1972463ea814d93522ef41
Anatoli Kuzmiankou <Anatoli_Kuzmiankou@epam.com>
2018-02-21 19:43:09 +0300

    Add theme

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import { Provider } from 'react-redux';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import configureStore from './store';
+import theme from './themes/main-theme';
 import './index.css';
 
 const store = configureStore();
 
 ReactDOM.render(
-  <MuiThemeProvider>
+  <MuiThemeProvider theme={ getMuiTheme(theme) }>
     <Provider store={ store }>
       <App />
     </Provider>
